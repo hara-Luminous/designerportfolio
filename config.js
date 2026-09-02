763dcf555ef2b8e6f2aa36a91ce06cf8aa27a694
hara-Luminous <hara@luminous-inc.jp>
2026-09-02 10:26:36 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // anon key はブラウザ公開用のキーです。service_role key は絶対に入れないでください。
 window.PORTFOLIO_SUPABASE = {
   url: 'https://jvjdipayuxuooydeoboh.supabase.co',
-  anonKey: 'sb_publishable_C5jJt6jNJ2xbgIIiYqQM_Q_qa33hEWz'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2amRpcGF5dXh1b295ZGVvYm9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyNTgwNDUsImV4cCI6MjEwMzgzNDA0NX0.IRRGnPvf7WnSFmJ0DtU9ZjhMA143Z2OoDwBPDNiYUrs'
 };
